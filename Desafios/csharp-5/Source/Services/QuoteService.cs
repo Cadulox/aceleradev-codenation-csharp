f5e26821969717efccbe2dd29b904685bdc8a313
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-04-16 17:07:37 -0300

    Monty Python phrase generator

---
+++ Desafios/csharp-5/Source/Services/QuoteService.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Codenation.Challenge.Models;
+
+namespace Codenation.Challenge.Services
+{
+    public class QuoteService : IQuoteService
+    {
+        private ScriptsContext _context;
+        private IRandomService _randomService;
+
+        public QuoteService(ScriptsContext context, IRandomService randomService)
+        {
+            this._context = context;
+            this._randomService = randomService;
+        }
+
+        public Quote GetAnyQuote()
+        {
+            int random = _randomService.RandomInteger(_context.Quotes.Count());
+            return _context.Quotes.ToList().ElementAtOrDefault(random);
+        }
+
+        public Quote GetAnyQuote(string actor)
+        {
+            List<Quote> quotes = _context.Quotes.Where(q => q.Actor == actor).ToList();
+
+            int random = _randomService.RandomInteger(quotes.Count());
+            return quotes.ElementAtOrDefault(random);
+        }
+    }
+}
